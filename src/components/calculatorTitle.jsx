f6f2d5f5e096364f6f253e909cef71f27adc5e88
Singhnav13 <navjot131@gmail.com>
2021-04-02 16:36:31 +0530

    All components created without css and logic functionality

---
+++ src/components/calculatorTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CalculatorTitle = (props) => {
+    return (
+        <div className="claculator-title">
+            { props.value }
+        </div>
+    )
+}
+
+export default CalculatorTitle;
---
+++ src/components/outputScreenRow.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+  
+const OutputScreenRow = () => {
+  return (
+    <div className="screen-row">
+      <input type="text" readOnly/>
+    </div>
+  )
+}
+  
+// Export Output Screen Row.
+export default OutputScreenRow;
